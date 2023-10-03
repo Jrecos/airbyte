9edeffbd92ea29f7f5d32495c3a61e71f7967938
jhonatan reyes <jhonatan.reyes@teaminternational.com>
2023-10-03 17:54:58 -0500

    baseline

--- run-ab-platform.sh
+++ run-ab-platform.sh
@@ -42,7 +42,7 @@ docker_compose_debug_yaml="docker-compose.debug.yaml"
                      flags="flags.yml"
              temporal_yaml="temporal/dynamicconfig/development.yaml"
 # any string is an array to POSIX shell. Space seperates values
-all_files="$docker_compose_yaml $docker_compose_debug_yaml $dot_env $dot_env_dev $flags $temporal_yaml"
+all_files="$docker_compose_debug_yaml $dot_env $dot_env_dev $flags $temporal_yaml"
 
 base_github_url="https://raw.githubusercontent.com/airbytehq/airbyte-platform/v$VERSION/"
 
